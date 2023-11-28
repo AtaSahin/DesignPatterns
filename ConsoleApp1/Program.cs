5bc5ce133fba7f6e5ae62444071cfab76b5fe6e0
Ata Sahin <atasahin05@gmail.com>
2023-11-28 11:53:14 +0300

    singleton

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public sealed class Singleton
+{
+
+    private static Singleton instance;
+
+
+    private static readonly object lockObject = new object();
+
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton Instance
+    {
+        get
+        {
+
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+
+    public void DisplayMessage()
+    {
+        Console.WriteLine("Singleton example usage");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Singleton singletonInstance = Singleton.Instance;
+
+
+        singletonInstance.DisplayMessage();
+
+
+        Singleton anotherInstance = Singleton.Instance;
+
+
+        if (singletonInstance == anotherInstance)
+        {
+            Console.WriteLine("2 instances are the same time of Singleton class");
+        }
+
+        Console.ReadLine();
+    }
+}
